142aa7e524eca3cd1ff80dfd613c3340aa246db7
Shulha Yahya <shulha.yahya@azavista.com>
2023-11-09 18:40:31 +0700

    add typograpgy colors

--- src/components/MuiComponentSamples/Samples/Typography.tsx
+++ src/components/MuiComponentSamples/Samples/Typography.tsx
@@ -4,54 +4,100 @@ import React from "react";
 
 export default function TypographyExample() {
   return (
-    <Box sx={{ width: 1, maxWidth: 800 }}>
-      <Typography variant="h1" component="h2" gutterBottom>
-        h1. Heading
-      </Typography>
-      <Typography variant="h2" gutterBottom>
-        h2. Heading
-      </Typography>
-      <Typography variant="h3" gutterBottom>
-        h3. Heading
-      </Typography>
-      <Typography variant="h4" gutterBottom>
-        h4. Heading
-      </Typography>
-      <Typography variant="h5" gutterBottom>
-        h5. Heading
-      </Typography>
-      <Typography variant="h6" gutterBottom>
-        h6. Heading
-      </Typography>
-      <Typography variant="subtitle1" gutterBottom>
-        subtitle1. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-        Quos blanditiis tenetur
-      </Typography>
-      <Typography variant="subtitle2" gutterBottom>
-        subtitle2. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-        Quos blanditiis tenetur
-      </Typography>
-      <Typography variant="body1" gutterBottom>
-        body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
-        blanditiis tenetur unde suscipit, quam beatae rerum inventore
-        consectetur, neque doloribus, cupiditate numquam dignissimos laborum
-        fugiat deleniti? Eum quasi quidem quibusdam.
-      </Typography>
-      <Typography variant="body2" gutterBottom>
-        body2. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
-        blanditiis tenetur unde suscipit, quam beatae rerum inventore
-        consectetur, neque doloribus, cupiditate numquam dignissimos laborum
-        fugiat deleniti? Eum quasi quidem quibusdam.
-      </Typography>
-      <Typography variant="button" display="block" gutterBottom>
-        button text
-      </Typography>
-      <Typography variant="caption" display="block" gutterBottom>
-        caption text
-      </Typography>
-      <Typography variant="overline" display="block" gutterBottom>
-        overline text
-      </Typography>
-    </Box>
+    <>
+      <Typography variant="h6">Typography Variant</Typography>
+      <Box sx={{ width: 1, maxWidth: 800 }}>
+        <Typography variant="h1" component="h2" gutterBottom>
+          h1. Heading
+        </Typography>
+        <Typography variant="h2" gutterBottom>
+          h2. Heading
+        </Typography>
+        <Typography variant="h3" gutterBottom>
+          h3. Heading
+        </Typography>
+        <Typography variant="h4" gutterBottom>
+          h4. Heading
+        </Typography>
+        <Typography variant="h5" gutterBottom>
+          h5. Heading
+        </Typography>
+        <Typography variant="h6" gutterBottom>
+          h6. Heading
+        </Typography>
+        <Typography variant="subtitle1" gutterBottom>
+          subtitle1. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          Quos blanditiis tenetur
+        </Typography>
+        <Typography variant="subtitle2" gutterBottom>
+          subtitle2. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          Quos blanditiis tenetur
+        </Typography>
+        <Typography variant="body1" gutterBottom>
+          body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
+          blanditiis tenetur unde suscipit, quam beatae rerum inventore
+          consectetur, neque doloribus, cupiditate numquam dignissimos laborum
+          fugiat deleniti? Eum quasi quidem quibusdam.
+        </Typography>
+        <Typography variant="body2" gutterBottom>
+          body2. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
+          blanditiis tenetur unde suscipit, quam beatae rerum inventore
+          consectetur, neque doloribus, cupiditate numquam dignissimos laborum
+          fugiat deleniti? Eum quasi quidem quibusdam.
+        </Typography>
+        <Typography variant="button" display="block" gutterBottom>
+          button text
+        </Typography>
+        <Typography variant="caption" display="block" gutterBottom>
+          caption text
+        </Typography>
+        <Typography variant="overline" display="block" gutterBottom>
+          overline text
+        </Typography>
+      </Box>
+
+      <Typography variant="h6">Typography color</Typography>
+      <Box sx={{ width: 1, maxWidth: 800 }}>
+        <Typography variant="subtitle1" gutterBottom color={"secondary"}>
+          subtitle1. Color secondary. Lorem ipsum dolor sit amet, consectetur
+          adipisicing elit. Quos blanditiis tenetur
+        </Typography>
+        <Typography variant="subtitle2" gutterBottom>
+          subtitle2. color default. Lorem ipsum dolor sit amet, consectetur
+          adipisicing elit. Quos blanditiis tenetur
+        </Typography>
+        <Typography variant="body1" gutterBottom color={"error"}>
+          body1. color error. Lorem ipsum dolor sit amet, consectetur
+          adipisicing elit. Quos blanditiis tenetur unde suscipit, quam beatae
+          rerum inventore consectetur, neque doloribus, cupiditate numquam
+          dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.
+        </Typography>
+        <Typography variant="body2" gutterBottom color={"primary"}>
+          body2. color primary. Lorem ipsum dolor sit amet, consectetur
+          adipisicing elit. Quos blanditiis tenetur unde suscipit, quam beatae
+          rerum inventore consectetur, neque doloribus, cupiditate numquam
+          dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.
+        </Typography>
+        <Typography
+          variant="button"
+          display="block"
+          gutterBottom
+          color={"secondary"}
+        >
+          color secondary. button text
+        </Typography>
+        <Typography variant="caption" display="block" gutterBottom>
+          color default. caption text
+        </Typography>
+        <Typography
+          variant="overline"
+          display="block"
+          gutterBottom
+          color={"error"}
+        >
+          overline text. color error
+        </Typography>
+      </Box>
+    </>
   )
 }
